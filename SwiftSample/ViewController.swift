b6de1bb803800fb3e7c26196f07326b5c4406191
higra-shi <cawasemi81@gmail.com>
2014-06-29 08:09:28 +0900

    commit

--- SwiftSample/ViewController.swift
+++ SwiftSample/ViewController.swift
@@ -13,6 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var viewFrame: CGRect = self.view.frame;
+
+        viewFrame.origin = CGPointZero;
+        viewFrame.size.height = 128;
+        var textLabel: UILabel = UILabel(frame: viewFrame);
+        textLabel.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5);
+        textLabel.autoresizingMask = UIViewAutoresizing.FlexibleWidth;
+        self.view.addSubview(textLabel);
+
+        viewFrame.origin.y += viewFrame.size.height + 8;
+        textLabel = UILabel(frame: viewFrame);
+        textLabel.backgroundColor = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5);
+        self.view.addSubview(textLabel);
     }
 
     override func didReceiveMemoryWarning() {
